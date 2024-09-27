6dbb5a09546eada613425273e124b7bf4d55019b
srinanoo <srinanoo@gmail.com>
2024-09-27 12:13:12 +0530

    27-09-2024

---
+++ reactjs/first-cra-proj/src/App.js
@@ -0,0 +1,91 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="row1">
+        <div className="row1section1">
+            <div>
+                <img src="images/Dinesh.jpg" alt="" className="logo" />
+            </div>
+            <div style={{"backgroundColor": "red", "color": "white"}}>
+                <h3>Dinesh</h3>
+                <div>Technical Trainer</div>
+                <div>Web Developer</div>
+                <div>Email Me</div>
+                <i class="fa-brands fa-facebook"></i>
+            </div>
+        </div>
+        <div className="row1section2">
+            <div>Projects</div>
+            <div>About Me</div>
+            <div>Contact Me</div>
+        </div>
+    </div>
+    <div className="row2">
+        <h3>Projects / Experiences</h3>
+        <div className="row2section1">
+            <div>
+                <img src="images/background.jpeg" alt="" /><br />
+                Project 1<br />
+                Project Description goes here...
+            </div>
+            <div>
+                <img src="images/background.jpeg" alt="" /><br />
+                Project 1<br />
+                Project Description goes here...
+            </div>
+            <div>
+                <img src="images/background.jpeg" alt="" /><br />
+                Project 1<br />
+                Project Description goes here...
+            </div>
+            <div>
+                <img src="images/background.jpeg" alt="" /><br />
+                Project 1<br />
+                Project Description goes here...
+            </div>
+            <div>
+                <img src="images/background.jpeg" alt="" /><br />
+                Project 1<br />
+                Project Description goes here...
+            </div>
+            <div>
+                <img src="images/background.jpeg" alt="" /><br />
+                Project 1<br />
+                Project Description goes here...
+            </div>
+        </div>
+    </div>
+    <div className="row3">
+        <h3>About Me</h3>
+        <div className="row3section1">
+            <div>
+                <h4>Personal Details</h4>
+                <ul>
+                    <li>ITEM</li>
+                    <li>ITEM</li>
+                    <li>ITEM</li>
+                    <li>ITEM</li>
+                </ul>
+            </div>
+            <div>
+                <h4>Educational Details</h4>
+                <ul>
+                    <li>ITEM</li>
+                    <li>ITEM</li>
+                    <li>ITEM</li>
+                    <li>ITEM</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div className="row4">
+        <div>&copy; Copyright. All Rights Reserved.</div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
